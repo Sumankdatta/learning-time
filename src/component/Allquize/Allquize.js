c5c76ba971ea6b4fd564819d0187cbcbb4d52ce2
Sumankdatta <sumankdatta18@gmail.com>
2022-10-12 12:12:10 +0600

    quiz section added

--- src/component/Allquize/Allquize.js
+++ src/component/Allquize/Allquize.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import './Allquize.css';
 
-const Allquize = ({quiz}) => {
+const Allquize = ({ quiz }) => {
     console.log(quiz)
-    const {question,id,options,total,name}=quiz;
+    const { question, id, options, total, name } = quiz;
+    console.log(options)
     return (
 
         <div className='quiz-data'>
-            
-            <p>Quiz no :{question.slice(3,-4)}</p>
-            <div  className='option'>
-            <p>{options}</p>
+
+            <div className='question'><h6>Quiz no :{question.slice(3, -4)}</h6></div>
+            <div className='display'>
+
+                <div className='label'>
+                    <input type="radio" id="html" name="fav_language" value="HTML"></input>
+                    <label for="html">{options[0]}</label>
+                </div>
+                <div className='label'>
+                    <input type="radio" id="html" name="fav_language" value="HTML"></input>
+                    <label for="html">{options[1]}</label>
+                </div><div className='label'>
+                    <input type="radio" id="html" name="fav_language" value="HTML"></input>
+                    <label for="html">{options[2]}</label>
+                </div><div className='label'>
+                    <input type="radio" id="html" name="fav_language" value="HTML"></input>
+                    <label for="html">{options[3]}</label>
+                </div>
             </div>
-            
+
         </div>
     );
 };
